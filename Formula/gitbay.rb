b0a97df1857c104f7c125d028cedbcc3fe9838a8
Christian Cleberg <hello@cleberg.net>
2026-08-25 16:15:44 -0500

    gitbay 0.4.0

--- Formula/gitbay.rb
+++ Formula/gitbay.rb
@@ -2,8 +2,8 @@ class Gitbay < Formula
   desc "CLI for gitbay, the CLI-first git forge"
   homepage "https://gitbay.org"
   url "https://gitbay.org/krz/gitbay.git",
-      tag:      "v0.3.0",
-      revision: "6b01ef3788b21e3a4c027a2afe7e7a5bda9ca000"
+      tag:      "v0.4.0",
+      revision: "b4684c088d2b99f0f42c4e95231c4c0edc122323"
   license "0BSD"
   head "https://gitbay.org/krz/gitbay.git", branch: "main"
 
