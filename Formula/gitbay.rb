045131f1d991a991b323b06281ee1937c84162f1
Christian Cleberg <hello@cleberg.net>
2026-09-18 23:08:31 -0500

    gitbay, gitbay-runner 1.28.0

--- Formula/gitbay.rb
+++ Formula/gitbay.rb
@@ -2,8 +2,8 @@ class Gitbay < Formula
   desc "CLI for gitbay, the CLI-first git forge"
   homepage "https://gitbay.org"
   url "https://gitbay.org/krz/gitbay.git",
-      tag:      "v1.27.0",
-      revision: "98b0b03d448a247f3d4284ddb51fefeea04ecbdd"
+      tag:      "v1.28.0",
+      revision: "e0fcc7c7b1279a04ace60aeaedace2b4c5eb4b30"
   license "0BSD"
   head "https://gitbay.org/krz/gitbay.git", branch: "main"
 
